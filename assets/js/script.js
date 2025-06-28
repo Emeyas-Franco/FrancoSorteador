ef25447310cc814b0e9df2d4aac711fa0d5084cf
Emeyas Franco <emeyasdev@gmail.com>
2025-06-28 16:18:26 -0300

    Ajustes de resultado e estilização

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,22 @@
 const sortear = document.getElementById("sortear")
 
 function sorteador(min, max) {
+
   min = parseInt(document.querySelector("#input-min").value)
   max = parseInt(document.querySelector("#input-max").value)
   const h2Result = document.querySelector("#h2result")
 
-  const result = Math.floor(Math.random() * (max - min + 1)) + min
-  
-  h2Result.innerHTML = `<h2>O número sorteado é: ${result}</h2>`  
 
+  if (!isNaN(min) && !isNaN(max)) {
+    if (min > max) {
+      h2Result.innerHTML = `<h2>O valor mínimo não pode ser maior que o máximo!</h2>`
+      return
+    }
+    const result = Math.floor(Math.random() * (max - min + 1)) + min
+    h2Result.innerHTML = `<h2>O número sorteado é: ${result}</h2>`
+  } else {
+    h2Result.innerHTML = `<h2>Por favor, preencha os dois campos com números válidos.</h2>`
+  }
 }
 
 sortear.addEventListener('click', sorteador)
